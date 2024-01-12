4a5a7a2c50f36374df2f984e7091e078a38f707d
trankhanh040147 <trankhanh040147@gmail.com>
2024-01-13 05:22:11 +0700

    3.5: Phonebook backend step5

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 import express from 'express'
 
 const app = express()
+// json-parser
+app.use(express.json())
+
+const generateId = () => {
+    // generate random num
+    let randomNum
+    const maxNum = 10000
+
+    // if the array is full, return -1
+    if(persons.length === maxNum){
+        return -1
+    }
+
+    while (true) {
+        randomNum = Math.floor(Math.random() * 10000) + 1
+        if(persons.find(person => person.id === randomNum) === undefined){
+            return randomNum
+        }
+    }
+}
 
 let persons = [
     {
@@ -45,6 +65,25 @@ app.get('/info', (request, response) => {
     // There can only be one response.send() statement in an Express app route. Once you send a response to the client using response.send(), 
     // the request-response cycle is complete and no further response can be sent.
     response.send(`Phonebook has info for ${persons.length} people <br> ${new Date()}`)
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    console.log(body);
+    if (!body) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
