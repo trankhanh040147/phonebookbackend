29ae472ffb827166b99fb5982e000e89dc8ece4e
trankhanh040147 <trankhanh040147@gmail.com>
2024-01-12 22:03:22 +0700

    3.2: Phonebook backend step2

--- index.js
+++ index.js
@@ -30,6 +30,12 @@ app.get('/api/persons', (request, response) => {
     console.log(persons);
 })
 
+app.get('/info', (request, response) => {
+    // There can only be one response.send() statement in an Express app route. Once you send a response to the client using response.send(), 
+    // the request-response cycle is complete and no further response can be sent.
+    response.send(`Phonebook has info for ${persons.length} people <br> ${new Date()}`)
+})
+
 // open port and start
 const PORT = 3001
 app.listen(PORT, () => {
